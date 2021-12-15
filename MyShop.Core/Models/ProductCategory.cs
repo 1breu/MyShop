4c9222e23d80c9512822955263a5662dd240d441
Andres Breucop <github.com/1breu>
2021-12-15 12:58:28 -0800

    First attempt at learning MVC and ASP.NET, by following a tutorial course from Brett Hargreaves (https://bretthargreaves.com/). Original project repo here: https://github.com/completecoder/MyShop/tree/master/MyShop

---
+++ MyShop.Core/Models/ProductCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyShop.Core.Models
+{
+    public class ProductCategory : BaseEntity
+    {
+        private string category;
+
+        public string Category { get => category; set => category = value; }
+    }
+}
---
+++ MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+namespace MyShop.WebUI.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ProductCategoryManagerController : Controller
+    {
+        IRepository<ProductCategory> context;
+
+        public ProductCategoryManagerController(IRepository<ProductCategory> productCategoryContext)
+        {
+            this.context = productCategoryContext;
+
+        }
+        // GET: ProductCategory
+        public ActionResult Index()
+        {
+            List<ProductCategory> productCategories = context.Collection().ToList();
+            return View(productCategories);
+        }
+
+        public ActionResult Create()
+        {
+            ProductCategory pCategory = new ProductCategory();
+            return View(pCategory);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCat)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productCat);
+            }
+            else
+            {
+                context.Insert(productCat);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            ProductCategory category = context.Find(Id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productCat, string Id)
+        {
+            ProductCategory categoryToEdit = context.Find(Id);
+            if (categoryToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(productCat);
+                }
+                categoryToEdit.Category = productCat.Category;
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            ProductCategory categoryToDelete = context.Find(Id);
+            if (categoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(categoryToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            ProductCategory categoryToDelete = context.Find(Id);
+            if (categoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
